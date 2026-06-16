9dedcb35c4b323dc18177e742e835cbd3ce187a6
Pedro Carnio | Shadow_Voidh <shadow.voidh@gmail.com>
2026-06-16 08:40:08 +0000

    Fix Py

--- dashboard/main.py
+++ dashboard/main.py
@@ -84,5 +84,6 @@ def rota_login():
 
 if __name__ == '__main__':
     inicializar_banco()
-   
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
